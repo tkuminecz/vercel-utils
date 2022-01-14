89cba6c70545ca8cf364eab06189f43562756c89
Tim Kuminecz <tkuminecz@gmail.com>
2022-01-14 01:22:36 -0500

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,92 +1,13 @@
-const path = require('path');
+"use strict"
 
 module.exports = {
-  extends: ['typescript', 'typescript/prettier'],
-  plugins: ['jest'],
-  env: {
-    es6: true,
-    node: true,
-  },
-  rules: {
-    'no-console': ['error', { allow: ['error'] }],
-    'prettier/prettier': ['error', { singleQuote: true, arrowParens: 'avoid' }],
-    quotes: ['error', 'single', { allowTemplateLiterals: true }],
-  },
-};
-
-/*
-module.exports = {
-  // So parent files don't get applied
-  root: true,
-  globals: {
-    preval: false,
-  },
   extends: [
-    'airbnb-typescript',
-    'plugin:jest/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    "@tkuminecz/eslint-config/typescript",
+    "@tkuminecz/eslint-config/prettier",
   ],
-  plugins: ['babel', 'import', 'jest'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: '2019',
-    sourceType: 'module',
-    ecmaFeatures: {
-      modules: true,
-    },
-    project: path.resolve(__dirname, './tsconfig.json'),
-  },
+  plugins: ["jest"],
   env: {
     es6: true,
     node: true,
-    'jest/globals': true,
   },
-  settings: {
-    'import/resolver': {
-      alias: {
-        map: [
-          ['~', path.resolve(__dirname, './src')],
-        ],
-        extensions: ['.ts', '.js', '.jsx', '.tsx', '.png'],
-      },
-    },
-  },
-  rules: {
-    'prefer-object-spread': 'error',
-    'no-underscore-dangle': 'off',
-    'consistent-this': ['error', 'self'],
-    'max-len': [
-      'error',
-      100,
-      2,
-      {
-        ignoreUrls: true,
-      },
-    ], // airbnb is allowing some edge cases
-    'no-console': ['error', { allow: ['error'] }], // airbnb is using warn
-
-    // Note, export default interface triggers error for ';', see https://github.com/typescript-eslint/typescript-eslint/issues/123
-    'no-extra-semi': 'off',
-    semi: ['error', 'always'],
-
-    'jest/valid-expect': 'off', // for jest-expect-message
-
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-
-    quotes: ['error', 'single', { allowTemplateLiterals: true }],
-    'prettier/prettier': ['error', { singleQuote: true, arrowParens: 'avoid' }],
-    'import/prefer-default-export': 'off',
-    'import/no-named-as-default': 'off',
-    'import/no-cycle': 'off',
-    'no-void': 'off',
-    'class-methods-use-this': 'off',
-    'no-nested-ternary': 'off',
-    'max-classes-per-file': 'off'
-  },
-};
-*/
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import 'isomorphic-unfetch';
-import { IncomingMessage, ServerResponse } from 'http';
+import "isomorphic-unfetch"
+import { IncomingMessage, ServerResponse } from "http"
 
 interface DocumentContext {
-  req: IncomingMessage;
-  res: ServerResponse;
+  req: IncomingMessage
+  res: ServerResponse
 }
 
 export const getCanonicalHost = async (
@@ -19,41 +19,41 @@ export const getCanonicalHost = async (
           Authorization: `Bearer ${apiToken}`,
         },
       }
-    );
+    )
 
     if (res.ok) {
-      const deployInfo = await res.json();
-      const [firstAlias] = deployInfo?.alias || [];
-      if (firstAlias) return firstAlias;
+      const deployInfo = await res.json()
+      const [firstAlias] = deployInfo?.alias || []
+      if (firstAlias) return firstAlias
     } else {
       // eslint-disable-next-line no-console
       console.warn(
         `Problem fetching deployment info for url ${vercelUrl}: ` +
           `API returned HTTP ${res.status} ${res.statusText}`
-      );
+      )
     }
   }
 
   // we couldn't find an alias, just use the given Vercel URL
-  return vercelUrl;
-};
+  return vercelUrl
+}
 
 export const getBaseUrl = (
   canonicalHost: string,
   useHttps?: boolean
 ): string => {
-  const protocol = useHttps ? 'https://' : 'http://';
-  return `${protocol}${canonicalHost}`;
-};
+  const protocol = useHttps ? "https://" : "http://"
+  return `${protocol}${canonicalHost}`
+}
 
 export const getCanonicalUrl = async (
   baseUrl: string,
   apiToken: string,
   useHttps?: boolean
 ): Promise<string> => {
-  const canonicalHost = await getCanonicalHost(baseUrl, apiToken);
-  return getBaseUrl(canonicalHost, useHttps);
-};
+  const canonicalHost = await getCanonicalHost(baseUrl, apiToken)
+  return getBaseUrl(canonicalHost, useHttps)
+}
 
 export const redirectToCanonicalUrl = async (
   ctx: DocumentContext,
@@ -64,19 +64,19 @@ export const redirectToCanonicalUrl = async (
   // only attempt to redirect to canonical URL if we have
   // a Vercel URL, a Vercel API token and we're server-side
   if (vercelUrl && apiToken && ctx.req) {
-    const currentHost = ctx.req.headers.host;
-    const canonicalHost = await getCanonicalHost(vercelUrl, apiToken);
+    const currentHost = ctx.req.headers.host
+    const canonicalHost = await getCanonicalHost(vercelUrl, apiToken)
     if (currentHost !== canonicalHost) {
       // we're not on the canonical url, redirect
-      const destUrl = `${getBaseUrl(canonicalHost, useHttps)}${ctx.req.url}`;
+      const destUrl = `${getBaseUrl(canonicalHost, useHttps)}${ctx.req.url}`
       // eslint-disable-next-line no-console
       console.warn(
         `Not at canonical url (${currentHost}), redirecting to ${destUrl}`
-      );
+      )
       ctx.res.writeHead(302, {
         Location: destUrl,
-      });
-      ctx.res.end();
+      })
+      ctx.res.end()
     }
   }
-};
+}
